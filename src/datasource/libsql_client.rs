44ec5ab4d270323810cf0820a2dfef0067d07896
Edgardo <edrayoca@gmail.com>
2024-01-09 15:29:30 -0500

    fix: version

--- src/datasource/libsql_client.rs
+++ src/datasource/libsql_client.rs
@@ -1,18 +1,8 @@
 #[cfg(feature = "libsql_error")]
-use libsql::Error as LibSqlError;
 use libsql::errors::{Error};
-
 
 #[allow(unused)]
 use crate::CrabError;
-
-
-#[cfg(feature = "libsql_error")]
-impl From<LibSqlError> for CrabError {
-    fn from(e: LibSqlError) -> Self {
-        CrabError::DatasourceError(e.to_string())
-    }
-}
 
 #[cfg(feature = "libsql_error")]
 impl From<Error> for CrabError {
